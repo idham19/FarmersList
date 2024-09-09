623ec19489c311536ede34c539576d9f3d018089
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-09 15:33:12 -0400

    rewardPoints method added to Customer Class

--- Customer.js
+++ Customer.js
@@ -1,19 +1,25 @@
-const Cart = require("./Cart")
+const Cart = require("./Cart");
 
-class Customer{
-constructor(name,email,shippingAddress){
-    this.name=name;
-    this.email=email;
-    this.shippingAddress=shippingAddress;
-    this.orderHistory=[]
+class Customer {
+  constructor(name, email, shippingAddress) {
+    this.name = name;
+    this.email = email;
+    this.shippingAddress = shippingAddress;
+    this.orderHistory = [];
+    this.rewardPoints = 0;
+  }
+  addToOrderHistory(cart) {
+    if (cart instanceof Cart) {
+      this.orderHistory.push(cart);
+    }
+  }
+  getRewardPoints() {
+    this.rewardPoints=0
+    for (let i = 0; i < this.orderHistory.length; i++) {
+      this.rewardPoints += this.orderHistory[i].rewardPoints;
+    }
+    return this.rewardPoints;
+  }
 }
-addToOrderHistory(cart){
- if(cart instanceof Cart){
-  this.orderHistory.push(cart)
- }
-}
-}
 
-
-
-module.exports=Customer
+module.exports = Customer;
--- Product.js
+++ Product.js
@@ -2,16 +2,18 @@ const { Cart } = require(".");
 
 // 
 class Product {
-    constructor(name, price, description) {
+    constructor(name, price, description,rewardPoints) {
       this.name = name;
       this.price = price;
       this.description = description;
       this.inStock = true;
+      this.rewardPoints=rewardPoints
     }
   
     display() {
       return `Name: ${this.name}, Price: $${this.price}, Description: ${this.description}`;
     }
+    
   }
   
   module.exports = Product;
